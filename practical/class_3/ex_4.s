20a09ed6597eb487ad4030d4495b1459731490df
Gonçalo Silva <43342715+TheGoncaloSilva@users.noreply.github.com>
2023-10-14 11:02:43 +0100

    updates

--- practical/class_3/ex_4.s
+++ practical/class_3/ex_4.s
@@ -4,23 +4,40 @@ nelem:	.word 	10			; array size
         .text
         .global main
 
-main:	addi 	r1,r0,nelem		; r1 = nelem
-        lw 	r2,0(r1)		; r2 = values[]
+main:	addi 	r1,r0,nelem		; r1 = *nelem
+        lw 	r1,0(r1)		; r1 = nelem
+	addi	r2,r0,values		; r2 = *values
         addi 	r3,r0,1			; r3 = i
         add 	r4,r0,r0		; r4 = j
         add 	r5,r0,r0		; r5 = tmp
-for1: 	slt 	r6,r5,r1		; r6 = i < n
-        beqz 	r6,endfor1		; for(i=1;i<n;i++){ -- tem q ter 3 nops em cada branch
+
+for: 	slt 	r6,r3,r1		; !r6 = i < n
+        beqz 	r6,endfor		; for(i=1;i<n;i++){ -- tem q ter 3 nops em cada branch
         lw 	r5,0(r2)		; tmp = arr[i]
-        addi 	r3,r3,1			; i++
-for2:
-
-        j 	for2
-endfor2:				; }
-	j 	for1				
-endfor1:				; }
-
-
+	addi 	r4,r3,-1		; j = i - 1
+	
+while: 	addi	r9,r0,-1		; !r9 = -1
+	sgt	r6,r4,r9		; !r6 = j>=0
+	beqz    r6, endW            ; while (j >= 0) {
+	addi 	r7,r0,4			; !r7 = 4
+	mult 	r7,r4,r7		; !r7 = j * 4
+	addi	r7,r7,values		; !r7 = *values[j]
+	lw	r7,0(r7)		; !r7 = values[j]
+	slt	r8,r7,r5		; !r8 = arr[j] < tmp
+	and 	r8,r7,r8		; !r7 = j >= 0 && arr[j] < tmp
+	bnez	r8,endW			; while (j >= 0 && arr[j] < tmp) {
+	addi 	r4,r4,1			; j = j + 1
+	addi 	r6,r0,4			; !r6 = 4
+	mult 	r6,r4,r6		; !r6 = j * 4
+	addi	r6,r6,values		; !r6 = *values[j]
+	sw	0(r6),r7		; arr[j + 1] = arr[j]
+	addi	r4,r4,-2		; j = j - 2
+        j 	while
+endW:					; }
+	addi 	r3,r3,1			; i++
+	addi	r2,r2,4			; r2 = *values[i++]
+	j 	for				
+endfor:					; }
 end:    trap	0			; end of program
 	
 
--- practical/class_3/insertion_sort.c
+++ practical/class_3/insertion_sort.c
@@ -1,13 +1,45 @@
-//! Attention, just demo code
-int main(){
-    int arr[] = {1,2,3,4,5,6,7,8,9,10};
-    int n = 10, key_to_insert;
-    int i, j, tmp;
+// C program for insertion sort -> orders the values of an 
+//  integer array stored in memory from the largest value 
+//  to the smallest one
+#include <math.h>
+#include <stdio.h>
+ 
+/* Function to sort an array using insertion sort*/
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
     for (i = 1; i < n; i++) {
-        tmp = arr[i];
-        for (j = i - 1; j >= 0 && arr[j] > key_to_insert; j--) {
+        key = arr[i];
+        j = i - 1;
+ 
+        /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+        while (j >= 0 && arr[j] < key) {
             arr[j + 1] = arr[j];
+            j = j - 1;
         }
-        arr[j + 1] = key_to_insert;
+        arr[j + 1] = key;
     }
+}
+ 
+// A utility function to print an array of size n
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+/* Driver program to test insertion sort */
+int main()
+{
+    int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    insertionSort(arr, n);
+    printArray(arr, n);
+ 
+    return 0;
 }
